f0d0ec1d20a1b9f54d36cafc2b7634e694623640
Olivier Labayle <olabayle@gmail.com>
2025-01-21 15:18:16 +0000

    add some julia deps to start mocking

--- src/SequantialGWAS.jl
+++ src/SequantialGWAS.jl
@@ -1,3 +1,11 @@
 module SequantialGWAS
 
+using CSV
+using DataFrames
+using DelimitedFiles
+# using SnpArrays
+# using CairoMakie
+
+include("mock.jl")
+
 end
---
+++ src/mock.jl
@@ -0,0 +1,5 @@
+function mock_genotyping_arrays(release_r8, release_2021_2023, release_2024_now)
+    for file in (release_r8, release_2021_2023, release_2024_now)
+        println("Mocking genotyping array: $file")
+    end
+end
