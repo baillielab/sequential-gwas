22d9792429d98e08729613b0d09cd96705bf98f3
Olivier Labayle <olabayle@gmail.com>
2025-03-11 20:35:04 +0000

    update qc from kgp

--- src/qc_from_kgp.jl
+++ src/qc_from_kgp.jl
@@ -1,4 +1,21 @@
 
+function get_kgp_ref_alt(kgp)
+    kgp_info = SequentialGWAS.load_variants_info(kgp)
+    kgp_ref_alt = Dict()
+    for variant_id in kgp_info.VARIANT_ID
+        chr, pos, ref, alt = split(variant_id, ":")
+        if length(ref) == length(alt) == 1
+            key = (chr, parse(Int, pos))
+            if haskey(kgp_ref_alt, key)
+                kgp_ref_alt[key] = nothing
+            else
+                kgp_ref_alt[key] = (ref, alt)
+            end
+        end
+    end
+    return kgp_ref_alt
+end
+
 almost_balanced(freq; threshold=0.02) = abs(freq - 0.5) < threshold
 
 """
@@ -16,14 +33,17 @@ Some particularly unexpected ACTION (REASON) are:
 
 Because they mean the minor/major alleles are reversed in our dataset as compared to the reference KGP.
 """
-function get_action(row; threshold=0.2)
-    if row.KGP_VARIANT_ID === missing
+function get_action(row, kgp_info; threshold=0.2)
+    key = (row.CHR_CODE, row.BP_COORD)
+    if !haskey(kgp_info, key)
         return "DROP (VARIANT-NOT-IN-KGP)"
     end
-    if row.NON_BIALLELIC === true
+    
+    if kgp_info[key] === nothing
         return "DROP (VARIANT-NON-BIALLELIC)"
     end
-    chr, pos, ref, alt = split(row.KGP_VARIANT_ID, ":")
+
+    ref, alt = kgp_info[key]
     # Note: PLINK2 ALLELE_1 and ALLELE_2 are the minor and major alleles, respectively
     # Ideal case, where the variant in our dataset matches the KGP dataset
     if row.ALLELE_1 == alt && row.ALLELE_2 == ref
@@ -36,7 +56,7 @@ function get_action(row; threshold=0.2)
     elseif row.ALLELE_1 == ref && row.ALLELE_2 == alt
         alt_ref_set = Set([ref, alt])
         if alt_ref_set == Set(["A", "T"]) || alt_ref_set == Set(["C", "G"])
-            if almost_balanced(row.ALLELE_1_FREQ; threshold=threshold) && almost_balanced(row.KGP_ALLELE_1_FREQ; threshold=threshold)
+            if almost_balanced(row.ALLELE_1_FREQ; threshold=threshold)
                 return "DROP (PALINDROMIC-MINOR-MAJOR-REVERSED-ALT-REF-BALANCED)"
             else
                 return "FLIP (PALINDROMIC-MINOR-MAJOR-REVERSED-ALT-REF)"
@@ -59,39 +79,37 @@ function get_action(row; threshold=0.2)
     end
 end
 
-function set_action_column!(variants_info::DataFrame; threshold=0.2)
+function set_action_column!(variants_info::DataFrame, kgp_info; threshold=0.2)
     variants_info.ACTION = map(eachrow(variants_info)) do row
-        get_action(row; threshold=threshold)
+        get_action(row, kgp_info; threshold=threshold)
     end
 end
 
-function set_action_column(variants_info, kgp_info; threshold=0.2)
-    variants_info = leftjoin(
-        variants_info, 
-        kgp_info, 
-        on=[:CHR_CODE, :BP_COORD]
-    )
-    # Non unique variants are non-biallelic, we will drop them
-    variants_info.NON_BIALLELIC = nonunique(
-        variants_info, 
-        [:CHR_CODE, :BP_COORD],
-        keep = :noduplicates
-    )
-    variants_info = unique(
-        variants_info, 
-        [:CHR_CODE, :BP_COORD], 
-        keep=:first
-    )
-    set_action_column!(variants_info; threshold=threshold)
-    return variants_info
+function set_new_id_column!(variants_info::DataFrame, kgp_info)
+    variants_info.NEW_VARIANT_ID = map(eachrow(variants_info)) do row
+        key = (row.CHR_CODE, row.BP_COORD)
+        if haskey(kgp_info, key) && kgp_info[key] !== nothing
+            # Thew new id is made from chr:pos:ref:alt
+            ref, alt = kgp_info[key]
+            return string(row.CHR_CODE, ":", row.BP_COORD, ":", ref, ":", alt)
+        else
+            # The new id does not matter, this variant will be dropped
+            return row.VARIANT_ID
+        end
+    end
+end
+
+function set_new_columns!(variants_info::DataFrame, kgp_info; threshold=0.2)
+    set_action_column!(variants_info, kgp_info; threshold=threshold)
+    set_new_id_column!(variants_info, kgp_info)
 end
 
 function generate_files_from_actions(outdir, prefixes_and_bims)
     # Get and write shared variants present in all files.
     variants_intersection = intersect(
-        filter(:ACTION => !startswith("DROP"), prefixes_and_bims[:release_r8][2]).KGP_VARIANT_ID,
-        filter(:ACTION => !startswith("DROP"), prefixes_and_bims[:release_2021_2023][2]).KGP_VARIANT_ID,
-        filter(:ACTION => !startswith("DROP"), prefixes_and_bims[:release_2024_now][2]).KGP_VARIANT_ID
+        filter(:ACTION => !startswith("DROP"), prefixes_and_bims[:release_r8][2]).NEW_VARIANT_ID,
+        filter(:ACTION => !startswith("DROP"), prefixes_and_bims[:release_2021_2023][2]).NEW_VARIANT_ID,
+        filter(:ACTION => !startswith("DROP"), prefixes_and_bims[:release_2024_now][2]).NEW_VARIANT_ID
     )
     ## For plink
     CSV.write(
@@ -120,14 +138,7 @@ function generate_files_from_actions(outdir, prefixes_and_bims)
             bim
         )
         # Write new bim file, replace missing by chr:bp:all1:all2, they will be dropped downstream
-        new_bim = select(bim, :CHR_CODE, :KGP_VARIANT_ID, :POSITION, :BP_COORD, :ALLELE_1, :ALLELE_2)
-        new_bim.KGP_VARIANT_ID = map(eachrow(new_bim)) do row
-            if row.KGP_VARIANT_ID === missing
-                return string(row.CHR_CODE, ":", row.BP_COORD, ":", row.ALLELE_1, ":", row.ALLELE_2)
-            else
-                return row.KGP_VARIANT_ID
-            end
-        end
+        new_bim = select(bim, :CHR_CODE, :NEW_VARIANT_ID, :POSITION, :BP_COORD, :ALLELE_1, :ALLELE_2)
         CSV.write(
             joinpath(outdir, string(prefix, ".new.bim")),
             new_bim, 
@@ -136,12 +147,12 @@ function generate_files_from_actions(outdir, prefixes_and_bims)
         )
         # Write flip lists
         to_flip = filter(
-            x -> startswith(x.ACTION, "FLIP") && x.KGP_VARIANT_ID in variants_intersection, 
+            x -> startswith(x.ACTION, "FLIP") && x.NEW_VARIANT_ID in variants_intersection, 
             bim
         )
         CSV.write(
             joinpath(outdir, string(prefix, ".flip.txt")), 
-            select(to_flip, [:KGP_VARIANT_ID]),
+            select(to_flip, [:NEW_VARIANT_ID]),
             header=false
         )
     end
@@ -150,12 +161,13 @@ end
 function load_variants_info(prefix)
     bim = SequentialGWAS.read_bim(string(prefix, ".bim"))
     afreq = CSV.read(string(prefix, ".acount"), DataFrame)
-    afreq.ALT_FREQS .= afreq.ALT_CTS ./ afreq.OBS_CT
-    return innerjoin(
-        bim, 
-        select(afreq, :ID =>:VARIANT_ID, :ALT_FREQS => :ALLELE_1_FREQ), 
-        on=:VARIANT_ID
+    alt_freqs = Dict(
+        zip(afreq.ID, afreq.ALT_CTS ./ afreq.OBS_CT)
     )
+    bim.ALLELE_1_FREQ = map(bim.VARIANT_ID) do id
+        get(alt_freqs, id, missing)
+    end
+    return bim
 end
 
 """
@@ -190,18 +202,12 @@ function generate_qc_extraction_files_from_kgp(
     release_r8_info = SequentialGWAS.load_variants_info(release_r8)
     release_2021_2023_info = SequentialGWAS.load_variants_info(release_2021_2023)
     release_2024_now_info = SequentialGWAS.load_variants_info(release_2024_now)
-    kgp_info = SequentialGWAS.load_variants_info(kgp)
-    rename!(kgp_info, 
-        :VARIANT_ID => :KGP_VARIANT_ID, 
-        :POSITION => :KGP_POSITION,
-        :ALLELE_1 => :KGP_ALLELE_1,
-        :ALLELE_2 => :KGP_ALLELE_2,
-        :ALLELE_1_FREQ => :KGP_ALLELE_1_FREQ
-    )
+    kgp_info = SequentialGWAS.get_kgp_ref_alt(kgp)
+
     # Format the chromosome and set the action column
-    release_r8_info = set_action_column(release_r8_info, kgp_info; threshold=threshold)
-    release_2021_2023_info = set_action_column(release_2021_2023_info, kgp_info; threshold=threshold)
-    release_2024_now_info = set_action_column(release_2024_now_info, kgp_info; threshold=threshold)
+    set_new_columns!(release_r8_info, kgp_info; threshold=threshold)
+    set_new_columns!(release_2021_2023_info, kgp_info; threshold=threshold)
+    set_new_columns!(release_2024_now_info, kgp_info; threshold=threshold)
     # Write variants related files to be used by plink:flip, drop
     prefixes_and_bims = Dict(
         :release_r8 => (basename(release_r8), release_r8_info),
--- test/qc_from_kgp.jl
+++ test/qc_from_kgp.jl
@@ -8,19 +8,24 @@ using DataFrames
 TESTDIR = joinpath(pkgdir(SequentialGWAS), "test")
 
 @testset "Test get_action" begin
+    kgp_info = Dict(
+        ("chr1", 1) => nothing,
+        ("chr1", 2) => ("A", "C"),
+        ("chr1", 3) => ("T", "A"),
+    )
     # After merging with KGP, if the variant is not in KGP, 
     # it will have a missing VARIANT_ID
-    row = (KGP_VARIANT_ID = missing, )
-    @test SequentialGWAS.get_action(row) == "DROP (VARIANT-NOT-IN-KGP)"
+    row = (CHR_CODE = "chr1", BP_COORD=0)
+    @test SequentialGWAS.get_action(row, kgp_info) == "DROP (VARIANT-NOT-IN-KGP)"
     # We only keep biallelic variants for now
-    row = (KGP_VARIANT_ID = "chr1:1:A:C", NON_BIALLELIC = true, ) 
-    @test SequentialGWAS.get_action(row) == "DROP (VARIANT-NON-BIALLELIC)"
+    row = (CHR_CODE = "chr1", BP_COORD=1)
+    @test SequentialGWAS.get_action(row, kgp_info) == "DROP (VARIANT-NON-BIALLELIC)"
     # If the reference allele is ALLELE_2 (major according to plink) and 
     # the alternative allele is ALLELE_2 (minor according to plink) then all is fine
-    row = (KGP_VARIANT_ID = "chr1:1:A:C", NON_BIALLELIC = false, ALLELE_1 = "C", ALLELE_2 = "A")
-    @test SequentialGWAS.get_action(row) == "KEEP (MINOR-MAJOR-MATCHING-ALT-REF)"
-    row = (KGP_VARIANT_ID = "chr1:1:T:A", NON_BIALLELIC = false, ALLELE_1 = "A", ALLELE_2 = "T")
-    @test SequentialGWAS.get_action(row) == "KEEP (MINOR-MAJOR-MATCHING-ALT-REF)"
+    row = (CHR_CODE = "chr1", BP_COORD=2, ALLELE_1 = "C", ALLELE_2 = "A")
+    @test SequentialGWAS.get_action(row, kgp_info) == "KEEP (MINOR-MAJOR-MATCHING-ALT-REF)"
+    row = (CHR_CODE = "chr1", BP_COORD=3, ALLELE_1 = "A", ALLELE_2 = "T")
+    @test SequentialGWAS.get_action(row, kgp_info) == "KEEP (MINOR-MAJOR-MATCHING-ALT-REF)"
 
     # There are two cases:
     ## (i) The variant is palindromic, then either:
@@ -28,15 +33,20 @@ TESTDIR = joinpath(pkgdir(SequentialGWAS), "test")
     ##  - It has opposite allele frequency in our dataset (this should only happen when MAF ≈ 0.5 and we drop these cases)
     ## (ii) The variant is not palyndromic, it has an opposite allele frequency in our dataset (likely when MAF ≈ 0.5), we annotate it
     ## Non palindromic variant
-    row = (KGP_VARIANT_ID = "chr1:1:C:A", NON_BIALLELIC = false, ALLELE_1 = "C", ALLELE_2 = "A")
-    @test SequentialGWAS.get_action(row) == "KEEP (MINOR-MAJOR-REVERSED-ALT-REF)"
+    kgp_info = Dict(
+        ("chr1", 1) => ("C", "A"),
+        ("chr1", 2) => ("T", "A"),
+        ("chr1", 3) => ("C", "G")
+    )
+    row = (CHR_CODE = "chr1", BP_COORD=1, ALLELE_1 = "C", ALLELE_2 = "A")
+    @test SequentialGWAS.get_action(row, kgp_info) == "KEEP (MINOR-MAJOR-REVERSED-ALT-REF)"
     ## Palindromic variants
     ### Balanced frequencies
-    row = (KGP_VARIANT_ID = "chr1:1:T:A", NON_BIALLELIC = false, ALLELE_1 = "T", ALLELE_2 = "A", ALLELE_1_FREQ = 0.49, KGP_ALLELE_1_FREQ = 0.51)
-    @test SequentialGWAS.get_action(row) == "DROP (PALINDROMIC-MINOR-MAJOR-REVERSED-ALT-REF-BALANCED)"
+    row = (CHR_CODE = "chr1", BP_COORD=2, ALLELE_1 = "T", ALLELE_2 = "A", ALLELE_1_FREQ = 0.49)
+    @test SequentialGWAS.get_action(row, kgp_info) == "DROP (PALINDROMIC-MINOR-MAJOR-REVERSED-ALT-REF-BALANCED)"
     ### Unbalanced frequencies, likely to be flipped
-    row = (KGP_VARIANT_ID = "chr1:1:C:G", NON_BIALLELIC = false, ALLELE_1 = "C", ALLELE_2 = "G", ALLELE_1_FREQ = 0.3, KGP_ALLELE_1_FREQ = 0.7)
-    @test SequentialGWAS.get_action(row) == "FLIP (PALINDROMIC-MINOR-MAJOR-REVERSED-ALT-REF)"
+    row = (CHR_CODE = "chr1", BP_COORD=3, ALLELE_1 = "C", ALLELE_2 = "G", ALLELE_1_FREQ = 0.3)
+    @test SequentialGWAS.get_action(row, kgp_info) == "FLIP (PALINDROMIC-MINOR-MAJOR-REVERSED-ALT-REF)"
 
     # Otherwise, the alleles are non-matching the reference at all, 
     # this can be either because:
@@ -44,15 +54,21 @@ TESTDIR = joinpath(pkgdir(SequentialGWAS), "test")
     # (note again, the major/minor alleles may not correspond even once flipped and we report it)
     # - There is a problem with the reported alleles in the dataset and we drop the variant (this will also be reported)
     # These variants will be dropped
-    row = (KGP_VARIANT_ID = "chr1:1:A:C", NON_BIALLELIC = false, ALLELE_1 = "T", ALLELE_2 = "A")
-    @test SequentialGWAS.get_action(row) == "DROP (ALLELES-NOT-MATCHING-KGP)"
-        row = (KGP_VARIANT_ID = "chr1:1:G:A", NON_BIALLELIC = false, ALLELE_1 = "G", ALLELE_2 = "C")
-    @test SequentialGWAS.get_action(row) == "DROP (ALLELES-NOT-MATCHING-KGP)"
+    kgp_info = Dict(
+        ("chr1", 1) => ("A", "C"),
+        ("chr1", 2) => ("G", "A"),
+        ("chr1", 3) => ("A", "C"),
+        ("chr1", 4) => ("T", "G")
+    )
+    row = (CHR_CODE = "chr1", BP_COORD=1, ALLELE_1 = "T", ALLELE_2 = "A")
+    @test SequentialGWAS.get_action(row, kgp_info) == "DROP (ALLELES-NOT-MATCHING-KGP)"
+    row = (CHR_CODE = "chr1", BP_COORD=2, ALLELE_1 = "G", ALLELE_2 = "C")
+    @test SequentialGWAS.get_action(row, kgp_info) == "DROP (ALLELES-NOT-MATCHING-KGP)"
     # These variants will be flipped
-    row = (KGP_VARIANT_ID = "chr1:1:A:C", NON_BIALLELIC = false, ALLELE_1 = "T", ALLELE_2 = "G")
-    @test SequentialGWAS.get_action(row) == "FLIP (COMPLEMENT-MINOR-MAJOR-REVERSED-ALT-REF)"
-    row = (KGP_VARIANT_ID = "chr1:1:T:G", NON_BIALLELIC = false, ALLELE_1 = "C", ALLELE_2 = "A")
-    @test SequentialGWAS.get_action(row) == "FLIP (COMPLEMENT)"
+    row = (CHR_CODE = "chr1", BP_COORD=3, ALLELE_1 = "T", ALLELE_2 = "G")
+    @test SequentialGWAS.get_action(row, kgp_info) == "FLIP (COMPLEMENT-MINOR-MAJOR-REVERSED-ALT-REF)"
+    row = (CHR_CODE = "chr1", BP_COORD=4, ALLELE_1 = "C", ALLELE_2 = "A")
+    @test SequentialGWAS.get_action(row, kgp_info) == "FLIP (COMPLEMENT)"
 end
 
 end
