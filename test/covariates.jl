9af9885252f7d564a41e54d3d23477f38558f081
Olivier Labayle <olabayle@gmail.com>
2025-04-15 09:27:52 +0100

    fix covariates tests

--- test/covariates.jl
+++ test/covariates.jl
@@ -19,8 +19,8 @@ TESTDIR = joinpath(PKGDIR, "test")
     copy!(ARGS, [
         "make-gwas-groups", 
         covariates_file,
-        inferred_covariates_file,
         variables_file,
+        "--inferred-covariates", inferred_covariates_file,
         "--output-prefix", output_prefix, 
         "--min-group-size", string(min_group_size)
     ])
@@ -31,27 +31,26 @@ TESTDIR = joinpath(PKGDIR, "test")
     ]
     @test_logs expected_logs... julia_main()
 
-    output_files = readdir(tmpdir)
-    groups = Set(getindex.(split.(basename.(output_files), "."), 3))
-    @test groups == Set(["ADMIXED", "AMR", "EUR", "SAS"])
-    variables = YAML.load_file(variables_file)
-    expected_covariates = ["FID", "IID", "AGE", "SEX", "AGE_x_SEX", "AGE_x_AGE", "PLATFORM__GSA-MD-24v3-0_A1", "PLATFORM__GSA-MD-48v4-0_A1"]
+    # Check covariate file
+    expected_covariate_cols = ["FID", "IID", "AGE", "SEX", "AGE_x_SEX", "AGE_x_AGE", "PLATFORM__GSA-MD-24v3-0_A1", "PLATFORM__GSA-MD-48v4-0_A1"]
+    covariates = CSV.read(joinpath(tmpdir, "group.covariates.csv"), DataFrame)
+    @test names(covariates) == expected_covariate_cols
+    @test !any(any(ismissing.(covariates[!, col])) for col in expected_covariate_cols)
+    @test nrow(covariates) > 2000
+    @test (covariates[!, "AGE_x_AGE"] == covariates[!, "AGE"] .* covariates[!, "AGE"])
+    @test (covariates[!, "AGE_x_SEX"] == covariates[!, "AGE"] .* covariates[!, "SEX"])
+    # Check phenotypes file
     expected_phenotypes = ["FID", "IID", "SEVERE_COVID_19"]
-    for group in groups
-        covariates = CSV.read(joinpath(tmpdir, "group.covariates.$group.csv"), DataFrame)
-        @test names(covariates) == expected_covariates
-        @test !any(any(ismissing.(covariates[!, col])) for col in expected_covariates)
-        @test nrow(covariates) >= min_group_size
-        @test (covariates[!, "AGE_x_AGE"] == covariates[!, "AGE"] .* covariates[!, "AGE"])
-        @test (covariates[!, "AGE_x_SEX"] == covariates[!, "AGE"] .* covariates[!, "SEX"])
-
-        phenotypes = CSV.read(joinpath(tmpdir, "group.phenotype.$group.csv"), DataFrame)
-        @test names(phenotypes) == expected_phenotypes
-        @test Set(phenotypes.SEVERE_COVID_19) == Set([0, 1])
-
+    phenotypes = CSV.read(joinpath(tmpdir, "group.phenotypes.csv"), DataFrame)
+    @test names(phenotypes) == expected_phenotypes
+    @test Set(phenotypes.SEVERE_COVID_19) == Set([0, 1])
+    @test nrow(phenotypes) == nrow(covariates)
+    # Check groups files
+    inferred_covariates = CSV.read(inferred_covariates_file, DataFrame)
+    for group in ["ADMIXED", "EUR", "AMR", "SAS"]
         individuals = CSV.read(joinpath(tmpdir, "group.individuals.$group.txt"), DataFrame; header=["FID", "IID"])
-        @test (individuals.FID == covariates.FID == phenotypes.FID)
-        @test (individuals.IID == covariates.IID == phenotypes.IID)
+        joined = innerjoin(inferred_covariates, individuals, on = [:FID, :IID])
+        @test all(==(group), joined.ANCESTRY_ESTIMATE)
     end
 end
 
@@ -59,34 +58,29 @@ end
     tmpdir = mktempdir()
     output_prefix = joinpath(tmpdir, "nogroup")
     covariates_file = joinpath(TESTDIR, "assets", "genomicc", "mock.covariates.csv")
-    inferred_covariates_file = joinpath(TESTDIR, "assets", "gwas", "covariates", "covariates.inferred_from_genotypes.csv")
-    variables_file = joinpath(TESTDIR, "assets", "gwas", "covariates", "variables_no_group.yaml")
+    variables_file = joinpath(TESTDIR, "assets", "gwas", "covariates", "variables_all.yaml")
     min_group_size = 100
     copy!(ARGS, [
         "make-gwas-groups", 
         covariates_file,
-        inferred_covariates_file,
         variables_file,
         "--output-prefix", output_prefix, 
         "--min-group-size", string(min_group_size)
     ])
     julia_main()
 
-    output_files = readdir(tmpdir)
     variables = YAML.load_file(variables_file)
-    expected_rows = 2148
-    covariates = CSV.read(joinpath(tmpdir, "nogroup.covariates.all.csv"), DataFrame)
+    covariates = CSV.read(joinpath(tmpdir, "nogroup.covariates.csv"), DataFrame)
     @test names(covariates) == ["FID", "IID", variables["covariates"]...]
-    @test nrow(covariates) == expected_rows
+    @test nrow(covariates) > 2000
 
-    phenotypes = CSV.read(joinpath(tmpdir, "nogroup.phenotype.all.csv"), DataFrame)
+    phenotypes = CSV.read(joinpath(tmpdir, "nogroup.phenotypes.csv"), DataFrame)
     @test names(phenotypes) == ["FID", "IID", variables["phenotypes"]...]
-    @test nrow(covariates) == expected_rows
+    @test nrow(phenotypes) == nrow(covariates)
 
     individuals = CSV.read(joinpath(tmpdir, "nogroup.individuals.all.txt"), DataFrame; header=["FID", "IID"])
     @test (individuals.FID == covariates.FID == phenotypes.FID)
     @test (individuals.IID == covariates.IID == phenotypes.IID)
-
 end
 
 end
